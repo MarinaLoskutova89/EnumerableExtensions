20f8d07a8fa7507957ac12d71cd5f618d058c672
Loskutova Marina <mloskutova1589@gmail.com>
2024-10-21 19:50:30 +0300

    fixed code

--- Top/EnumerableExtensions.cs
+++ Top/EnumerableExtensions.cs
@@ -11,22 +11,17 @@ namespace Top
         public static IEnumerable<T> Top<T>(this IEnumerable<T> input, int x)
         {
             if (x < 1 || x > 100) throw new ArgumentException("The value must be in the range from 1 to 100!");
-            
-            IEnumerable<T> reverseValues = input.Reverse();
+            if (input == null || input.Count() == 0) throw new Exception("A null or empty value was passed!");
+
             decimal finalIndex = Math.Ceiling(input.Count() * x / 100m);
-            IEnumerable<T>? output = reverseValues.Take((int)finalIndex);
+            IEnumerable<T>? output = input.TakeLast((int)finalIndex);
 
             return output;
         }
 
         public static IEnumerable<T> Top<T>(this IEnumerable<T> input, int x, Func<T, int> selector)
         {
-            if (x < 1 || x > 100) throw new ArgumentException("The value must be in the range from 1 to 100!");
-
-            IEnumerable<T> reverseValues = input.OrderByDescending(selector);
-            decimal finalIndex = Math.Ceiling(input.Count() * x / 100m);
-            IEnumerable<T>? output = reverseValues.Take((int)finalIndex);
-
+            IEnumerable<T> output = input.Top(x).OrderByDescending(selector);
             return output;
         }
     }
--- Top/Program.cs
+++ Top/Program.cs
@@ -6,32 +6,32 @@ namespace Top
     {
         static void Main(string[] args)
         {
-            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            Console.WriteLine($"[{string.Join(',', list.Top(30))}]");
-            Console.WriteLine();
-
-            List<Person> anotherList = new()
+            try
             {
-                new() { Name = "Alice", Age = 25 },
-                new() { Name = "Bob", Age = 15 },
-                new() { Name = "Charlie", Age = 40 },
-                new() { Name = "Dean", Age = 80 },
-                new() { Name = "Eva", Age = 5 },
-                new() { Name = "Frank", Age = 30 },
-                new() { Name = "Greg", Age = 55 },
-                new() { Name = "Hank", Age = 28 },
-                new() { Name = "Ilona", Age = 13 },
-            };
+                var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+                Console.WriteLine($"[{string.Join(',', list.Top(30))}]");
+                Console.WriteLine();
 
-            var output = anotherList.Top(30, person => person.Age);
-            output.ToList().ForEach(x => Console.WriteLine($"Name - {x.Name}, Age - {x.Age}"));
+                List<Person> anotherList = new()
+                {
+                    new() { Name = "Alice", Age = 25 },
+                    new() { Name = "Bob", Age = 15 },
+                    new() { Name = "Charlie", Age = 40 },
+                    new() { Name = "Dean", Age = 80 },
+                    new() { Name = "Eva", Age = 5 },
+                    new() { Name = "Frank", Age = 30 },
+                    new() { Name = "Greg", Age = 55 },
+                    new() { Name = "Hank", Age = 28 },
+                    new() { Name = "Ilona", Age = 13 },
+                };
 
+                var output = anotherList.Top(30, person => person.Age);
+                output.ToList().ForEach(x => Console.WriteLine($"Name - {x.Name}, Age - {x.Age}"));
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
-    }
-
-    class Person
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
     }
 }
